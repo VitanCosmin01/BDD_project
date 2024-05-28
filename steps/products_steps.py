fbf0e198e1eba3340bc032be158411dd0c16795a
VitanCosmin01 <137814408+VitanCosmin01@users.noreply.github.com>
2024-05-28 11:08:25 +0200

    BDD_project

---
+++ steps/products_steps.py
@@ -0,0 +1,45 @@
+from behave import *
+
+
+# Scenariu 1
+@given("I am on the product's page")
+def step_impl(context):
+    context.products_page.navigate_to_product_page()
+
+
+@when("I click on the product's name")
+def step_impl(context):
+    context.products_page.click_name()
+
+
+@then("I go to product's detailpage")
+def step_impl(context):
+    context.details_page.check_current_url()
+
+
+# Scenariul 2
+@when("I click on the product's picture")
+def step_impl(context):
+    context.products_page.click_picture()
+
+
+# Scenariu 3
+@when("I click the button Add to cart")
+def step_impl(context):
+    context.products_page.add_to_cart()
+
+
+@then("The product is added to the cart")
+def step_impl(context):
+    context.products_page.check_kart()
+
+
+# Scenariu 4
+@when("I click the button Remove")
+def step_impl(context):
+    context.products_page.remove_from_kart()
+
+
+@then("The item is deleted from kart")
+def step_impl(context):
+    context.products_page.check_empty_kart()
